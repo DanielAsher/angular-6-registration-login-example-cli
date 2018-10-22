d27a1af886f49b54bf9338e1c983bb02440db4ab
Daniel Asher <dan@lexilabs.co>
2018-10-22 17:05:17 +0100

    get authentication token

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -1,6 +1,7 @@
 ﻿import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map, concatMap } from 'rxjs/operators';
 
 import { environment } from '../../environments/environment';
 
@@ -8,17 +9,40 @@ import { environment } from '../../environments/environment';
 export class AuthenticationService {
     constructor(private http: HttpClient) { }
 
-    login(username: string, password: string) {
-        return this.http.post<any>(`${environment.apiUrl}/users/authenticate`, { username: username, password: password })
-            .pipe(map(user => {
+    login(username: string, password: string): Observable<any> {
+        return this.http.post<any>(`${environment.apiUrl}/auth`, 
+            {
+                "app_id":"",
+                "provider":"password",
+                "data": username,
+                "user_info": {
+                "register": false,
+                "password": password
+                }
+            })
+            .pipe(
+                concatMap( user => {
+                    return this.http.post<any>(`${environment.apiUrl}/auth`, 
+                    {
+                        "app_id":"",
+                        "provider":"realm", // Note provider is 'realm'
+                        "data": user.refresh_token.token, // Token from previous response
+                        "path":"books" // Path of the realm you want to access, e.g. '/user-id/tickets
+                    }
+                    )
+                }),
+                map(user => {
                 // login successful if there's a jwt token in the response
-                if (user && user.token) {
+                console.log("🔑 Auth response: ", user)
+                if (user && user.access_token) {
+                    user.username = username;
+                    user.password = password;
                     // store user details and jwt token in local storage to keep user logged in between page refreshes
                     localStorage.setItem('currentUser', JSON.stringify(user));
                 }
-
                 return user;
-            }));
+            }),
+            );
     }
 
     logout() {
