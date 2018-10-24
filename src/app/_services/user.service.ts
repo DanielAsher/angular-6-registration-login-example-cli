6c57470b8408eff1b64a72d2bae9da2bee11407b
Daniel Asher <dan@lexilabs.co>
2018-10-24 22:15:10 +0100

    comment out unused user service calls

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -13,23 +13,23 @@ export class UserService {
         return this.http.get<PageQuery>(`${environment.apiUrl}/graphql/test-gql${query}`)
     }
     
-    getAll() {
-        return this.http.get<User[]>(`${environment.apiUrl}/users`);
-    }
+    // getAll() {
+    //     return this.http.get<User[]>(`${environment.apiUrl}/users`);
+    // }
 
-    getById(id: number) {
-        return this.http.get(`${environment.apiUrl}/users/` + id);
-    }
+    // getById(id: number) {
+    //     return this.http.get(`${environment.apiUrl}/users/` + id);
+    // }
 
-    register(user: User) {
-        return this.http.post(`${environment.apiUrl}/users/register`, user);
-    }
+    // register(user: User) {
+    //     return this.http.post(`${environment.apiUrl}/users/register`, user);
+    // }
 
-    update(user: User) {
-        return this.http.put(`${environment.apiUrl}/users/` + user.id, user);
-    }
+    // update(user: User) {
+    //     return this.http.put(`${environment.apiUrl}/users/` + user.id, user);
+    // }
 
-    delete(id: number) {
-        return this.http.delete(`${environment.apiUrl}/users/` + id);
-    }
+    // delete(id: number) {
+    //     return this.http.delete(`${environment.apiUrl}/users/` + id);
+    // }
 }
