ac63ea72a4b9e1ae499ec8a47cb86dda190c2c54
Daniel Asher <dan@lexilabs.co>
2018-10-25 23:03:39 +0100

    Add graphQL subscriptions

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,13 +1,43 @@
-import {NgModule} from '@angular/core';
-import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
-import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
-import {InMemoryCache} from 'apollo-cache-inmemory';
-import {environment} from '../environments/environment'
+import { NgModule } from '@angular/core'
+import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular'
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http'
+import { WebSocketLink } from 'apollo-link-ws'
+import { split } from 'apollo-link'
+import { getMainDefinition } from 'apollo-utilities'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { environment } from '../environments/environment'
+import { longStackSupport } from 'q';
 
 const uri = `${environment.apiUrl}/graphql/test-gql`; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink) {
+
+  const httpLinkHandler = httpLink.create({uri})
+  // Create a WebSocket link:
+  const ws = new WebSocketLink({
+    uri: `wss://storysmarties.us1.cloud.realm.io:443/graphql/%2Ftest-gql`,
+    options: {
+      reconnect: true,
+      connectionParams: () => {
+        const user = JSON.parse(localStorage.getItem('currentUser'))
+        const token = user.access_token && user.access_token.token
+        if (token) {
+          return { token }
+        } else {
+          return { }
+        }
+      }
+    }
+  });
+  const link = split(
+    ({ query }) => {
+      const { kind, operation } = getMainDefinition(query);
+      return kind === 'OperationDefinition' && operation === 'subscription';
+    },
+    ws,
+    httpLinkHandler,
+  )
   return {
-    link: httpLink.create({uri}),
+    link: httpLinkHandler,
     cache: new InMemoryCache(),
   };
 }
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core'
-import { Apollo } from 'apollo-angular'
+import { Apollo, QueryRef } from 'apollo-angular'
 import gql from 'graphql-tag'
 import { PageQuery, Page } from '../_models/page'
 import { Subscription, Observable } from 'rxjs'
@@ -13,7 +13,14 @@ const PagesOfPeterRabbit = gql`
     }
   }
 `
-
+const PagesOfPeterRabbitSub = gql`
+  subscription PagesOfPeterRabbit {
+    pages(query: "") {
+      uuid
+      text
+    }
+  }
+`
 
 @Component({
   selector: 'app-pages',
@@ -21,6 +28,7 @@ const PagesOfPeterRabbit = gql`
   styleUrls: ['./pages.component.css']
 })
 export class PagesComponent implements OnInit, OnDestroy {
+  pagesQuery: QueryRef<any>
   pages$: Observable<any>
   queryResult: PageQuery
   pages: any[];
@@ -32,32 +40,51 @@ export class PagesComponent implements OnInit, OnDestroy {
   constructor(private apollo: Apollo) { }
 
   ngOnInit() {
-    this.querySubscription = this.apollo
-      .watchQuery({
-        query: PagesOfPeterRabbit
-      })
-      .valueChanges.subscribe(result => {
-        this.pages = result.data && (result.data as any).pages
-        this.loading = result.loading
-        this.error = result.errors
-      })
-    
-      this.pages$ = this.apollo
-          .watchQuery({
-            query: PagesOfPeterRabbit
-          })
-          .valueChanges
-          .pipe(map(({data}) => (data as any).pages))
-    setTimeout( () => {
-      console.log('🔃 Refresh PagesOfPeterRabbit query')
-      this.apollo
-          .watchQuery({
-            query: PagesOfPeterRabbit
-          }).valueChanges.pipe(map(({data}) => (data as any).pages)).subscribe()
-    }, 20000)
+    // this.querySubscription = this.apollo
+    //   .watchQuery({
+    //     query: PagesOfPeterRabbit
+    //   })
+    //   .valueChanges.subscribe(result => {
+    //     this.pages = result.data && (result.data as any).pages
+    //     this.loading = result.loading
+    //     this.error = result.errors
+    //   })
+    this.pagesQuery = this.apollo
+        .watchQuery({
+          query: PagesOfPeterRabbit
+        })
+        
+    this.pages$ = this.pagesQuery
+        .valueChanges
+        .pipe(map(({data}) => (data as any).pages))
+
+    this.pagesQuery.subscribeToMore({
+      document: PagesOfPeterRabbitSub,
+      updateQuery: (prev, { subscriptionData}) => {
+        if (!subscriptionData.data) {
+          return prev
+        }
+
+        const newPages = subscriptionData.data.pages
+        return {
+          ...prev,
+          ...newPages
+        }
+      }
+    })
+
+    // setTimeout( () => {
+    //   console.log('🔃 Refresh PagesOfPeterRabbit query')
+    //   this.apollo
+    //       .watchQuery({
+    //         query: PagesOfPeterRabbit
+    //       }).valueChanges.pipe(map(({data}) => (data as any).pages)).subscribe()
+    // }, 20000)
 
   }
   ngOnDestroy() {
-    this.querySubscription.unsubscribe()
+    if (this.querySubscription) {
+      this.querySubscription.unsubscribe()
+    }
   }
 }
