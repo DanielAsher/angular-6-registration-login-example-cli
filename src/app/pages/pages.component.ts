5b55a0999d3a5c97201c1609ad50bfe46acd909e
Daniel Asher <dan@lexilabs.co>
2018-10-25 14:17:00 +0100

    Use apollo client to get pages from realm

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Apollo } from 'apollo-angular'
+import gql from 'graphql-tag'
+import { PageQuery, Page } from '../_models/page'
 
 @Component({
   selector: 'app-pages',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pages.component.css']
 })
 export class PagesComponent implements OnInit {
+  queryResult: PageQuery
+  pages: any[];
+  // {
+  //   return this.queryResult.data.pages
+  // }
+  loading = true
+  error: any
 
-  constructor() { }
+  constructor(private apollo: Apollo) { }
 
   ngOnInit() {
+    this.apollo
+      .watchQuery({
+        query: gql`
+          query PagesOfPeterRabbit {
+            pages(query: "") {
+              uuid
+              text
+            }
+          }
+        `
+      })
+      .valueChanges.subscribe(result => {
+        this.pages = result.data && result.data.pages
+        this.loading = result.loading
+        this.error = result.errors
+      })
   }
 
 }
