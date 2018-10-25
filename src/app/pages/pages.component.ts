1533692c39b8c52fb19bb3bf43473a2593865ba6
Daniel Asher <dan@lexilabs.co>
2018-10-25 14:51:48 +0100

    refactor graphql query

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,41 +1,46 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, OnDestroy } from '@angular/core'
 import { Apollo } from 'apollo-angular'
 import gql from 'graphql-tag'
 import { PageQuery, Page } from '../_models/page'
+import { Subscription } from 'rxjs';
+
+const PagesOfPeterRabbit = gql`
+  query PagesOfPeterRabbit {
+    pages(query: "") {
+      uuid
+      text
+    }
+  }
+`
+
 
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
   styleUrls: ['./pages.component.css']
 })
-export class PagesComponent implements OnInit {
+export class PagesComponent implements OnInit, OnDestroy {
   queryResult: PageQuery
   pages: any[];
-  // {
-  //   return this.queryResult.data.pages
-  // }
   loading = true
   error: any
+  
+  private querySubscription: Subscription
 
   constructor(private apollo: Apollo) { }
 
   ngOnInit() {
-    this.apollo
+    this.querySubscription = this.apollo
       .watchQuery({
-        query: gql`
-          query PagesOfPeterRabbit {
-            pages(query: "") {
-              uuid
-              text
-            }
-          }
-        `
+        query: PagesOfPeterRabbit
       })
       .valueChanges.subscribe(result => {
-        this.pages = result.data && result.data.pages
+        this.pages = result.data && (result.data as any).pages
         this.loading = result.loading
         this.error = result.errors
       })
   }
-
+  ngOnDestroy() {
+    this.querySubscription.unsubscribe()
+  }
 }
